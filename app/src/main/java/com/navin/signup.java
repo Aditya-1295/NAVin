feafa789986468595389dd9d4e73b2b2d43d5d27
aditya <adityaguptamessi@gmail.com>
2020-02-11 22:50:49 +0530

    ffdffgfg

--- app/src/main/java/com/navin/signup.java
+++ app/src/main/java/com/navin/signup.java
@@ -36,15 +36,7 @@ import com.google.firebase.database.ValueEventListener;
 public class signup extends AppCompatActivity {
     EditText editname,editemail,editpass,editphone;
     Button registerbtn,alreadybtn;
-    FirebaseAuth fAuth;
-
-    public static final String Firebase_Server_URL = "https://navin-dea57.firebaseio.com/";
     String nameholder,phoneholder;
-
-    Firebase firebase;
-    DatabaseReference databaseReference,rootRef,demoRef;
-
-     public static final String Database_Path = "User_Detail_Database";
 
 
 
@@ -53,110 +45,12 @@ public class signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        Firebase.setAndroidContext(signup.this);
-        firebase = new Firebase(Firebase_Server_URL);
-        databaseReference = FirebaseDatabase.getInstance().getReference(Database_Path);
         editname = findViewById(R.id.editname);
         editemail = findViewById(R.id.editemail);
         editpass = findViewById(R.id.editpassword);
         editphone = findViewById(R.id.editphone);
         registerbtn = findViewById(R.id.registerbtn);
         alreadybtn = findViewById(R.id.alreadybtn);
-        fAuth = FirebaseAuth.getInstance();
-
-
-
-
-
-
-
-        if (fAuth.getCurrentUser() != null){
-            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-            finish();
-        }
-
-
-        registerbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = editemail.getText().toString().trim();
-                String password = editpass.getText().toString().trim();
-
-                if (TextUtils.isEmpty(password)){
-                    editemail.setError("This field cannot be left empty");
-                    return;
-
-                }
-
-                if (TextUtils.isEmpty(email)){
-                    editpass.setError("This field cannot be left empty");
-                    return;
-                }
-
-                if (password.length()<6){
-                    editpass.setError("Password too short");
-                    return;
-                }
-
-                // registering the user to firebase;
-
-                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            Toast.makeText(signup.this, "Account Created", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        }
-                        else{
-                            Toast.makeText(signup.this, "Error please Try again "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
-                        }
-
-
-
-
-
-                    }
-                });
-
-
-
-                userDetails userDetail = new userDetails();
-
-
-                getDataFromEditText();
-
-                userDetail.setUserName(nameholder);
-                userDetail.setPhoneNumber(phoneholder);
-                String UserRecordIDFromServer = databaseReference.push().getKey();
-                databaseReference.child(UserRecordIDFromServer).setValue(userDetail);
-                Toast.makeText(signup.this, "sent to database",Toast.LENGTH_LONG).show();
-//                    rootRef = FirebaseDatabase.getInstance().getReference();
-//                    demoRef = rootRef.child("User_Detail_Database");
-//
-//
-//                    String name = editname.getText().toString();
-//                    String phone = editphone.getText().toString();
-//
-//                    demoRef.child("name").setValue(name);
-//                    demoRef.child("phoneNumber").setValue(phone);
-
-
-
-
-            }
-        });
-
-        alreadybtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),login.class));
-            }
-        });
-    }
-
-    private void getDataFromEditText() {
-         nameholder = editname.getText().toString().trim();
-         phoneholder = editphone.getText().toString().trim();
 
 
 
